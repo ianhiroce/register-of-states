936d3328f6801b8615edbf0fdaba1ae44340f679
ianhiroce <hirocex@gmail.com>
2024-11-03 19:49:15 -0300

    Changed Home file, and created Sidebar tsx/css components

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,25 @@
+import "./Sidebar.css"
+
+export function Sidebar() {
+  return (
+    <>
+      <h1>Nome da Empresa</h1>
+
+      <h3>Cadastros Comuns</h3>
+      <div>
+        <ul>
+          <li><a href="#">Países</a></li>
+          <li><a href="#">Estados</a></li>
+          <li><a href="#">Municípios</a></li>
+          <li><a href="#">Tipo de Logradouro</a></li>
+          <li><a href="#">Natureza Jurídica</a></li>
+          <li><a href="#">Atividade Econômica</a></li>
+          <li><a href="#">Regiões</a></li>
+        </ul>
+      </div>
+      
+      <h3>Grupo de Acessos</h3>
+      <h3>Usuários</h3>
+    </>
+  )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,7 @@
+import { Sidebar} from "../components/Sidebar"
+
 export function Home () {
   return (
-    <h1>Olá Mundo</h1>
+    <Sidebar />
   )
 }
