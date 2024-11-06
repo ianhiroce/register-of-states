fb3ba7b3ffeabcda5681dcc79c80ab4663e5d8af
ianhiroce <hirocex@gmail.com>
2024-11-06 08:34:43 -0300

    Initial commit page Register

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import { Header } from "../components/Header"
 import { Sidebar} from "../components/Sidebar"
 import { Table } from "../components/Table"
+import "./Home.css"
 
 export function Home () {
   return (
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,5 +1,33 @@
 export function Register() {
   return (
-    <h1>Page Register</h1>
+    <>
+      <form id="form" action="form">
+        <h3>Adicionar Estado</h3>
+        <div className="form-content">
+          <label htmlFor="code">Código</label>
+          <input
+          type="number"
+          placeholder="__"
+          maxLength={ 2 }
+          />
+          <label htmlFor="">UF</label>
+          <input
+          type="text"
+          placeholder="UF"
+          maxLength={ 2 }
+          />
+          <label htmlFor="">Nome</label>
+          <input
+          type="text"
+          placeholder="Informe o nome do estado"
+          />
+          <label htmlFor="">Região</label>
+          <input
+          type="text"
+          placeholder="Selecione"
+          />
+        </div>
+      </form>
+    </>
   )
 }
